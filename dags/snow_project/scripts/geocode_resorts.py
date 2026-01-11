548e02e7fe8ce8c33631e65cbb7fbb34cd7d4a3b
joanaand <joana.andriulyte@gmail.com>
2026-01-11 13:40:46 +0200

    Variable fix

--- dags/snow_project/scripts/geocode_resorts.py
+++ dags/snow_project/scripts/geocode_resorts.py
@@ -3,8 +3,10 @@ import pandas as pd
 import time
 import os
 from pathlib import Path
+from airflow.models import Variable
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
+#API_KEY = os.getenv("OPENWEATHER_API_KEY") - for testing locally
+API_KEY = Variable.get("OPENWEATHER_API_KEY")
 if not API_KEY:
     raise ValueError("OPENWEATHER_API_KEY not found in environment variables")
 
